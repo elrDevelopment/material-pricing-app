d15202637b767532ba769288fd6adf4538bcd712
erick@r10dev.com <erick@r10dev.com>
2020-10-17 08:00:49 -0500

    initial commit for tailwind setup

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+// postcss.config.js
+const autoprefixer = require('autoprefixer');
+const tailwindcss = require('tailwindcss');
+const tailwindui = require('@tailwindcss/ui');
+module.exports = {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    tailwindui
+  ],
+};
